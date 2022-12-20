c088cc0bfe1a8f683a5b716415a19289cc7bc737
Zuzanna Kaźmierczak <zusapphire@gmail.com>
2022-12-20 21:15:50 +0100

    Configure webpack

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+const path = require("path");
+
+module.exports = {
+    entry: "./src/index.js",
+    mode: "development",
+    output: {
+        filename: "main.js",
+        path: path.resolve(__dirname, "dist"),
+    },
+    devServer: {
+        static: {
+            directory: path.join(__dirname, "dist"),
+        },
+        compress: true,
+        port: 9000,
+    },
+    module: {
+        rules: [
+            {
+                test: /\.css$/,
+                use: ["style-loader", "css-loader"],
+            },
+        ],
+    },
+};
